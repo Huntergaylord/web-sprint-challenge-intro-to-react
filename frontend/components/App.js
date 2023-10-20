cd173b63254ffe88b310122b9f69ee5a578f9685
Hunter Gaylord <huntergaylord209@gmail.com>
2023-10-20 14:34:56 -0600

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Character from './Character'
+import Characters from './Character'
+
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
@@ -8,11 +9,38 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  const [characters, setCharacters] = useState([]);
+  const [planets, setPlanets] = useState([]);
+
+  // Create effects to fetch the data and put it in state
+  useEffect(() => {
+    // Fetch character data
+    axios.get(urlPeople)
+      .then((response) => {
+        setCharacters(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching character data:', error);
+      });
+
+    // Fetch planet data
+    axios.get(urlPlanets)
+      .then((response) => {
+        setPlanets(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching planet data:', error);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {characters.map((characterData) => (
+        <Characters key={characterData.id} data={characterData} planets={planets} />
+      ))}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character({ data }) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [showHomeworld, setShowHomeworld] = useState(false);
+
+  const toggleHomeworld = () => {
+    setShowHomeworld(!showHomeworld);
+  };
+
   return (
     <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    {/* Use the same markup with the same attributes as in the mock */}
+    <div className="character-card">
+    <h3 className="character-name">{data.name}</h3>
+      <button onClick={toggleHomeworld}>Toggle Homeworld</button>
+      {showHomeworld && (
+        <div className="homeworld">
+          <h4>Homeworld: {data.homeworld.name}</h4>
+          <p>Population: {data.homeworld.population}</p>
+        </div>
+      )}
     </div>
-  )
+  </div>
+);
 }
+  
+
 
 export default Character
